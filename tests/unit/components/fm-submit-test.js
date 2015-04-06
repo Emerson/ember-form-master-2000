bcdc8df616e13650ed74ab8660951f34b9ff9b10
Duy Nguyen <duynguyen0511@gmail.com>
2015-04-06 14:41:51 -0600

    Change submit tagName to button

--- tests/unit/components/fm-submit-test.js
+++ tests/unit/components/fm-submit-test.js
@@ -18,7 +18,7 @@ moduleForComponent('fm-submit', {
 test('fm-submit renders properly', function(assert) {
   var component = this.subject();
   this.render();
-  assert.equal(component.$('input:submit').length, 1, 'Renders submit element');
+  assert.equal(component.$('button:submit').length, 1, 'Renders submit element');
   assert.ok(component.$().hasClass('form-group'), 'Has the class of form-control');
 });
 
@@ -26,17 +26,17 @@ test('fm-submit allows custom class names on the input element', function(assert
   this.container.lookup('fmconfig:main').submitButtonClasses = ['button', 'another-class'];
   var component = this.subject();
   this.render();
-  assert.ok(component.$('input').hasClass('button'), 'It has the .button class');
-  assert.ok(component.$('input').hasClass('button'), 'It has the .another-class class');
+  assert.ok(component.$('button').hasClass('button'), 'It has the .button class');
+  assert.ok(component.$('button').hasClass('button'), 'It has the .another-class class');
 });
 
 test('fm-submit can be disabled', function(assert) {
   var component = this.subject();
   component.set('disabled', true);
   this.render();
-  assert.ok(component.$('input:disabled').length, 'It rendered a disabled submit');
+  assert.ok(component.$('button:disabled').length, 'It rendered a disabled submit');
   Ember.run(function() {
     component.set('disabled', false);
   });
-  assert.ok(!component.$('input:disabled').length, 'It rendered a submit that is not disabled');
+  assert.ok(!component.$('button:disabled').length, 'It rendered a submit that is not disabled');
 });
