8f9c8bc9553b45d8dcc7e56ab627a08ef652b1ed
Gordon Cassie <gordoncassie@gmail.com>
2016-04-07 11:35:09 -0400

    [Tweak] Update version number in the initializer.

--- app/initializers/ember-form-master-2000.js
+++ app/initializers/ember-form-master-2000.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 if(Ember.libraries) {
-  Ember.libraries.register('Ember Form Master 2000', '2.1.4');
+  Ember.libraries.register('Ember Form Master 2000', '3.0.0');
 }
 
 export { default, initialize } from 'ember-form-master-2000/initializers/ember-form-master-2000';
