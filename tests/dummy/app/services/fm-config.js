83d37069e0e5b03e0d75175995cc2f74b111cc85
Brent Jubinville <brent@marbletank.com>
2016-04-14 12:11:38 -0400

    chore(dummy app): add example for overriding config

---
+++ tests/dummy/app/services/fm-config.js
@@ -0,0 +1,13 @@
+import FormMaster2000ConfigService from 'ember-form-master-2000/services/fm-config';
+
+export default FormMaster2000ConfigService.extend({
+
+  // default config
+  // https://github.com/Emerson/ember-form-master-2000/blob/master/addon/services/fm-config.js
+
+  // e.g. semantic-ui style overrides
+  formClass: 'ui form',
+  wrapperClass: 'field',
+  errorClass: 'error'
+
+});
