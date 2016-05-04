a858b24992ce7a7f791d644f23075acd48a3c831
Emerson Lackey <emerson.lackey@gmail.com>
2016-05-04 12:53:44 -0400

    [BUMP] - Bumping minor version so we can publish to NPM

--- app/initializers/ember-form-master-2000.js
+++ app/initializers/ember-form-master-2000.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 if(Ember.libraries) {
-  Ember.libraries.register('Ember Form Master 2000', '3.0.0');
+  Ember.libraries.register('Ember Form Master 2000', '3.0.1');
 }
 
 export { default, initialize } from 'ember-form-master-2000/initializers/ember-form-master-2000';
