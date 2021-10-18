f0fe5387be06f2fe89638f5d7d033ce7ca90a036
Rachel Armstrong <rachel.armstrong@imanage.com>
2021-10-18 15:56:55 -0400

    deprecation workflow working

---
+++ tests/dummy/config/deprecation-workflow.js
@@ -0,0 +1,16 @@
+window.deprecationWorkflow = window.deprecationWorkflow || {};
+window.deprecationWorkflow.config = {
+  workflow: [
+    { handler: "silence", matchId: "ember-global" },
+    { handler: "silence", matchId: "ember.built-in-components.import" },
+    { handler: "silence", matchId: "ember.globals-resolver" },
+    { handler: "silence", matchId: "deprecated-run-loop-and-computed-dot-access" },
+    { handler: "silence", matchId: "ember-views.curly-components.jquery-element" },
+    { handler: "silence", matchId: "this-property-fallback" },
+    { handler: "silence", matchId: "ember-metal.get-with-default" },
+    { handler: "silence", matchId: "ember-component.send-action" },
+    { handler: "throw", matchId: "computed-property.override" },
+    { handler: "silence", matchId: "ember.built-in-components.legacy-arguments" },
+    { handler: "silence", matchId: "setting-on-hash" },
+  ]
+};
